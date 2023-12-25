2480341058139f175ee56245684c5c6d4628ca7c
Sean Nance <sean_nance@hotmail.com>
2023-12-25 14:19:01 -0800

    created script.js file

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Nest one Array within Another Array
+
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+
+This is also called a multi-dimensional array.
+
+Create a nested array called myArray.
+*/
+
+// Only change code below this line
+const myArray = [["socks", 12], ["shoes", 8]];
